ea83d44138f24cd99edc191253a8c57da76ac805
geneparcellano <gene.parcellano@gmail.com>
2018-10-19 07:21:23 -0500

    Improve layout, remove calculate button

--- src/components/calculator/Calculator.vue
+++ src/components/calculator/Calculator.vue
@@ -1,37 +1,56 @@
 <template>
 	<div id="calculator">
 		<h1>BAP</h1>
-		<ul class="brackets-list">
-			<li class="bracket-item" v-for="b in brackets">
-				<strong class="rate">{{ b.rate }}%</strong>
-				<span class="single">Single: {{ b.single }}</span>
-				<span class="joint">Joint: {{ b.joint }}</span>
-			</li>
-		</ul>
-		<div class="result" v-if="result">
-			<h2>Results</h2>
-			<div class="data-group">
-				<span>Buy again price: </span>
-				<strong>${{ result.bap }}</strong>
-			</div>
-			<div class="data-group">
-				<span>Pre-tax Gain: </span>
-				<span>${{ result.bap }}</span>
-			</div>
-			<div class="data-group">
-				<span>Tax amount: </span>
-				<span>${{ result.taxAmount }}</span>
-			</div>
-			<div class="data-group">
-				<span>Post-tax gain: </span>
-				<span>${{ result.postTax }}</span>
-			</div>
+		<div class="right">
+			<section class="brackets">
+				<ul class="brackets-list">
+					<li class="bracket-item" v-for="b in brackets">
+						<strong class="rate">{{ b.rate }}%</strong>
+						<span class="single">Single: {{ b.single }}</span>
+						<span class="joint">Joint: {{ b.joint }}</span>
+					</li>
+				</ul>
+			</section>
+			<section class="tax-info">
+				<h2>Tax Bracket</h2>
+				<div class="data-group">
+					<span>Tax bracket: </span>
+					<span>{{ taxBracket }}%</span>
+				</div>
+				<div class="data-group space">
+					<span>Capital Gain: </span>
+					<span>{{ longTerm ? 'Long Term' : 'Short Term' }}</span>
+				</div>
+				<div class="data-group">
+					<span>Tax Rate: </span>
+					<span>{{ taxRatePercent }}</span>
+				</div>
+			</section>
+			<section class="result">
+				<h2>Results</h2>
+				<div class="data-group">
+					<span>Pre-tax Gain: </span>
+					<span>{{ result.preTax | currency() }}</span>
+				</div>
+				<div class="data-group">
+					<span>Tax amount: </span>
+					<span>{{ result.taxAmount | currency() }}</span>
+				</div>
+				<div class="data-group">
+					<span>Post-tax gain: </span>
+					<span>{{ result.postTax | currency() }}</span>
+				</div>
+				<div class="data-group space">
+					<span>Buy again price: </span>
+					<strong>{{ result.bap | currency() }}</strong>
+				</div>
+			</section>
 		</div>
 		<form>
 			<div class="form-body">
 				<div class="input-group">
-					<label>Tax Bracket</label>
-					<input type="number" v-model="bracket">
+					<label>Annual Salary</label>
+					<input type="number" v-model="annualSalary">
 				</div>
 				<div class="input-group">
 					<label>Cost Basis</label>
@@ -51,8 +70,7 @@
 				</div>
 			</div>
 			<div class="form-foot">
-				<button type="button" class="primary" @click="clear">Clear</button>
-				<button type="button" @click="calculate">Calculate</button>
+				<button type="button" class="primary" @click="clear">Reset</button>
 			</div>
 		</form>
 	</div>
