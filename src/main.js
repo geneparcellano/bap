bb78d65b3bf41328319afc71e972f29a86ae4631
geneparcellano <gene.parcellano@gmail.com>
2018-10-19 07:21:02 -0500

    Applied vue2-filters

--- src/main.js
+++ src/main.js
@@ -1,7 +1,10 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vue2Filters from 'vue2-filters'
 
 Vue.config.productionTip = false
+
+Vue.use(Vue2Filters)
 
 new Vue({
   render: h => h(App)
